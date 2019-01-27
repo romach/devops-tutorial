4ac64c3ff7ea876a5cb6b17f93995ffc63be89f7
Roman Cherepanov <romach007@gmail.com>
2019-01-27 16:46:59 +0200

    Fix github username

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -26,7 +26,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'devops-tutorial',
-  organizationName: 'romach007',
+  organizationName: 'romach',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
